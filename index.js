b43e263d8d7c6d1ffd3cc345c852b5198ccfc8c3
Grigory Telenok <telenok.grigory@gmail.com>
2020-11-15 06:42:37 +1000

    Add body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const todoRoutes = require('./routes/todos');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +16,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
