8e9c964ee287778c821e79c5b5acd48a492bf972
Grigory Telenok <telenok.grigory@gmail.com>
2020-11-15 05:38:13 +1000

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -61,7 +61,9 @@ router.get('/create', (req, res) => {
 router.post('/compile_scss', async (req, res) => {
     const path = 'styles.scss';
 
-    fs.writeFile(path, req.body);
+    fs.writeFile(path, req.body, () => {
+        throw new Error('Ошибка при записи данных в файл');
+    });
 
     const compiledScss = new ScssScript(path).compileToCss();
 
