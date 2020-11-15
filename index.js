09d5447da7749c9b92883a37308294d508a16e68
Grigory Telenok <telenok.grigory@gmail.com>
2020-11-16 03:40:38 +1000

    Add hbs helper

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const hbs = exphbs.create({
     extname: 'hbs',
 });
 
+hbs.registerHelper('equals8', (value) => {
+    return value === 8;
+});
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
