2cf653afb8c40ad7d1faf5d7b4df47b73c372de0
Grigory Telenok <telenok.grigory@gmail.com>
2020-11-17 19:03:23 +1000

    Update project structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const miscRoutes = require('./routes/misc');
+const miscRoutes = require('./server/routes/misc');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
