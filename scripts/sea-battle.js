c10e1790f8cdbe5df80b36d261b6c30092fa071f
Grigory Telenok <telenok.grigory@gmail.com>
2021-01-24 18:39:30 +1000

    Minify vue

--- scripts/sea-battle.js
+++ scripts/sea-battle.js
@@ -1,4 +1,4 @@
-import Vue from 'vue/dist/vue';
+import Vue from 'vue/dist/vue.esm';
 import { EVENT_TYPES, CELL_STATES, SHIP_TYPES, GENERAL_SHIP_TYPES, STATUSES } from './constants';
 
 const wsAddress = 'wss://slider-constructor.herokuapp.com/sea-battle';
